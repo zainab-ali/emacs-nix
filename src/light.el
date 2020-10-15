721659fcdf3a8a29659efab88e75c137dbf9437a
zainab <zainab.ali.london@gmail.com>
2020-10-15 01:26:04 +0100

    Edit keyboard backlight

--- src/light.el
+++ src/light.el
@@ -6,9 +6,16 @@
   (shell-command (s-concat "light " arg-string))
   )
 
+(defun light-keyboard (arg-string)
+  (shell-command (s-concat "light -s sysfs/leds/tpacpi::kbd_backlight " arg-string))
+  )
+
 (defhydra hydra-light (global-map "<f5>")
   "brightness"
   ("<f5>" (light "-U 10") "dim")
-  ("<f6>" (light "-A 10") "brighten"))
+  ("<f6>" (light "-A 10") "brighten")
+  ("<f7>" (light-keyboard "-U 50") "kbd dim")
+  ("<f8>" (light-keyboard "-A 50") "kbd brighten")
+  )
 
 (provide 'light)
