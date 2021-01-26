caa26ab602cf4fd4d583bf8d9ed0426e061d001b
zainab <zainab.ali.london@gmail.com>
2021-01-26 21:38:51 +0000

    Export COE scratch to files

--- src/coe-code.el
+++ src/coe-code.el
@@ -101,9 +101,9 @@ columns instead."
           (funcall check-function))))))
 
 (defun coe-code--src-copy ()
-  "Copies the last src block in an code.org file.
+  "Copies the last src block in an org file.
 
-Inserts the block at the end of the code.org file."
+Inserts the block at the end of the org file."
   (let ((last-block nil))
     (org-element-map
         (org-element-parse-buffer)
@@ -118,7 +118,7 @@ Inserts the block at the end of the code.org file."
                (cdr last-block))))))
 
 
-;; Diff core
+;; Core
 
 (defun coe-code--overlays-between (begin end)
   "Overlays between two points."
@@ -143,18 +143,63 @@ in the body of a let block:
 The let and (x 1) expressions have been added and the entire block is
 surrounded."
   (let ((face (pcase type
-                ('add 'coe-code-face-add)
-                ('remove 'coe-code-face-remove)
-                ('replace 'coe-code-face-replace)
-                ('surround 'coe-code-face-surround)
-                ('omit 'coe-code-face-omit)))
+                ('add 'coe-code-face-diff-add)
+                ('remove 'coe-code-face-diff-remove)
+                ('replace 'coe-code-face-diff-replace)
+                ('surround 'coe-code-face-diff-surround)
+                ('omit 'coe-code-face-diff-omit)
+                ;; TODO : Should this be centralized?
+                ('region 'coe-code-face-scratch-region)
+                ('point 'coe-code-face-scratch-point)
+                (_ (error "Unrecognized type: %s" type))
+                ))
         (o (make-overlay begin end)))
     (overlay-put o 'coe-type type)
     (overlay-put o 'face face)
     (when priority (overlay-put o 'priority 1))))
 
+(defun coe-code--delete-overlays ()
+  "Delete the overlays."
+  (--each (coe-code--overlays) (delete-overlay it)))
 
-;; Diff persistence
+(defun coe-code-delete ()
+  "Delete the overlay at point."
+  (interactive)
+  (--each
+      (--filter (overlay-get it 'coe-type) (overlays-at (point)))
+    (delete-overlay it)))
+
+;; Persistence
+
+(defun coe-code--save (filename)
+  "Save the regions to a file."
+  (interactive)
+  (let* ((os (--filter (not (= (overlay-start it) (overlay-end it)))
+                       (coe-code--overlays)))
+         (vals (--sort
+                (< (caar it) (caar other))
+                (--map
+               (cons (cons (overlay-start it) (overlay-end it))
+                     (overlay-get it 'coe-type))
+               os))))
+    (unless (seq-empty-p vals)
+      (with-temp-buffer
+        (insert "(\n")
+        (--each vals (insert (format "%s\n" it)))
+        (insert ")")
+      (write-region nil nil filename)))))
+
+(defun coe-code--load (filename)
+  "Load the regions from a file."
+  (when (file-exists-p filename)
+    (let ((os (with-temp-buffer
+                (insert-file-contents filename)
+                (read (buffer-string)))))
+      (--each os
+        (pcase-let ((`((,start . ,end) . ,type) it))
+          (coe-code--overlay-insert start end type))))))
+
+;; Diff faces
 
 (defun coe-code--diff-filename (buf)
   "Computes the name of the diff.el file"
@@ -164,50 +209,22 @@ surrounded."
 (defun coe-code-save-diff ()
   "Save the diff regions to a file."
   (interactive)
-  (let* ((os (--filter (not (= (overlay-start it) (overlay-end it)))
-                       (coe-code--overlays)))
-         (vals (--sort
-                (< (caar it) (caar other))
-                (--map
-               (cons (cons (overlay-start it) (overlay-end it))
-                     (overlay-get it 'coe-type))
-               os)))
-        (filename (coe-code--diff-filename (current-buffer))))
-    (unless (seq-empty-p vals)
-      (with-temp-buffer
-        (insert "(\n")
-        (--each vals (insert (format "%s\n" it)))
-        (insert ")")
-      (write-region nil nil filename)))))
+  (coe-code--save (coe-code--diff-filename (current-buffer))))
 
 (defun coe-code--load-diff ()
   "Load the diff regions from a file."
-  (let ((diff-filename (coe-code--diff-filename (current-buffer))))
-    (when (file-exists-p diff-filename)
-      (let ((os (with-temp-buffer
-                  (insert-file-contents diff-filename)
-                  (read (buffer-string)))))
-        (--each os
-          (pcase-let ((`((,start . ,end) . ,type) it))
-            (coe-code--overlay-insert start end type)))))))
+  (coe-code--load (coe-code--diff-filename (current-buffer))))
 
-(defun coe-code--delete-overlays ()
-  "Delete the overlays."
-  (--each (coe-code--overlays) (delete-overlay it)))
-
-
-;; Diff faces
-
-(defface coe-code-face-add '((t :background "#507549"))
+(defface coe-code-face-diff-add '((t :background "#507549"))
   "The face for the added code overlay.")
 
-(defface coe-code-face-remove '((t :background "#754a49"))
+(defface coe-code-face-diff-remove '((t :background "#754a49"))
   "The face for the deleted code overlay.")
 
-(defface coe-code-face-replace '((t :background "#756649"))
+(defface coe-code-face-diff-replace '((t :background "#756649"))
   "The face for the replaced code overlay.")
 
-(defface coe-code-face-surround '((t :background "#614875"))
+(defface coe-code-face-diff-surround '((t :background "#614875"))
   "The face for the surrounded code overlay.")
 
 ;; Diff insertion
@@ -236,13 +253,6 @@ surrounded."
   (coe-code--overlay-insert begin end 'surround)
   (deactivate-mark))
 
-(defun coe-code-diff-delete ()
-  "Delete the overlay at point."
-  (interactive)
-  (--each
-      (--filter (overlay-get it 'coe-type) (overlays-at (point)))
-    (delete-overlay it)))
-
 ;; Omission
 
 ;; Omission face
@@ -256,9 +266,42 @@ surrounded."
   (coe-code--overlay-insert begin end 'omit)
   (deactivate-mark))
 
+;; Scratch
+
+(defface coe-code-face-scratch-region '((t :background "#797282"))
+  "The face for the region overlay.")
+
+(defface coe-code-face-scratch-point '((t :background "#614875"))
+  "The face for the point overlay.")
+
+(defun coe-code-scratch-region (begin end)
+  "Highlights a region as added code."
+  (interactive "r")
+  (coe-code--overlay-insert begin end 'region)
+  (deactivate-mark))
+
+(defun coe-code-scratch-point (point)
+  "Highlights a region as added code."
+  (interactive "d")
+  (coe-code--overlay-insert point (+ 1 point) 'point t))
+
+(defun coe-code--scratch-filename (buf)
+  "Computes the name of the scratch.el file"
+  (concat (file-name-directory (buffer-file-name buf))
+          "scratch.el"))
+
+(defun coe-code-save-scratch ()
+  "Save the scratch regions to a file."
+  (interactive)
+  (coe-code--save (coe-code--scratch-filename (current-buffer))))
+
+(defun coe-code--load-scratch ()
+  "Load the scratch regions from a file."
+  (coe-code--load (coe-code--scratch-filename (current-buffer))))
+
 ;; Export
 
-(defun coe-code--export-text ()
+(defun coe-code--export-text (export-function)
   "Return the marked up src block text at point."
   (let ((el (org-element-at-point)))
     (pcase el
@@ -283,12 +326,12 @@ surrounded."
            (emacs-lisp-mode)
            (indent-region (point-min) (point-max))
            (--each (coe-code--overlays)
-             (coe-code--export-overlay-to-text it))
+             (funcall export-function it))
            (coe-code--delete-overlays)
            (buffer-string))))
       (_ (message "No src block at point. Found type: %s" (car el))))))
 
-(defun coe-code--export-overlay-to-text (o)
+(defun coe-code--export-code-overlay (o)
   "Write an ovrelay to the current export buffer.
 
 The buffer is for export purposes only, so only contains code from a single
@@ -305,6 +348,22 @@ src-block. The overlaid text is surrounded by symbols depending on its type."
      (insert ?…))
     (_ )))
 
+(defun coe-code--export-scratch-overlay (o)
+  "Write an ovrelay to the current export buffer.
+
+The buffer is for export purposes only, so only contains code from a single
+src-block. The overlaid text is surrounded by symbols depending on its type."
+  (pcase (overlay-get o 'coe-type)
+    ('region
+     (goto-char (overlay-start o))
+     (insert ?⋘)
+     (goto-char (overlay-end o))
+     (insert ?⋙))
+    ('point
+     (goto-char (+ 1 (overlay-start o)))
+     (insert ?|))
+    (_ )))
+
 (defun coe-code--export-headlines ()
   "Return a list of all headlines with accompanying source positions."
   (org-element-map (org-element-parse-buffer)
@@ -317,23 +376,29 @@ src-block. The overlaid text is surrounded by symbols depending on its type."
                                       (org-element-contents section))))
         (cons text (org-element-property :begin src-block))))))
 
-(defun coe-code--code-filename (buf)
-  "Computes the name of the code.org file"
-  (concat (file-name-directory (buffer-file-name buf))
-          "code.org"))
-
-(defun coe-code-export ()
-  "Export all code snippets to a code directory."
-  (interactive)
+(defun coe-code--export (dirname export-function)
+  "Export all code snippets to a directory."
   (--each-indexed (coe-code--export-headlines)
     (pcase it
       (`(,name . ,point)
        (goto-char point)
-       (let ((text (coe-code--export-text)))
+       (let ((text (coe-code--export-text export-function)))
          (with-temp-buffer
            (insert text)
-           (write-file (concat default-directory "/code/"
-                               (number-to-string it-index) "-" name))))))))
+           (write-file (concat default-directory
+                               (format "/%s/" dirname)
+                               (number-to-string it-index)
+                               "-" name))))))))
+
+(defun coe-code-export-code ()
+  "Export all snippets to a 'code' directory."
+  (interactive)
+  (coe-code--export "code" #'coe-code--export-code-overlay))
+
+(defun coe-code-export-scratch ()
+  "Export all snippets to a 'scratch' directory."
+  (interactive)
+  (coe-code--export "scratch" #'coe-code--export-scratch-overlay))
 
 ;; Minor mode
 
@@ -349,16 +414,37 @@ src-block. The overlaid text is surrounded by symbols depending on its type."
     (,(kbd "C-c r") . ,#'coe-code-diff-replace)
     (,(kbd "C-c s") . ,#'coe-code-diff-surround)
     (,(kbd "C-c 0") . ,#'coe-code-omit)
-    (,(kbd "C-c e") . ,#'coe-code-export)
-    (,(kbd "C-c <backspace>") . ,#'coe-code-diff-delete))
+    (,(kbd "C-c e") . ,#'coe-code-export-code)
+    (,(kbd "C-c <backspace>") . ,#'coe-code-delete))
   (if coe-code-mode
       ;; Enable coe
       (progn (add-hook 'before-save-hook #'coe-code-save-diff nil t)
              (coe-code--load-diff))
     ;; Disable coe
-    (progn (coe-code--save-diff)
+    (progn (coe-code-save-diff)
            (coe-code--delete-overlays)
            (remove-hook 'before-save-hook #'coe-code-save-diff t))))
+
+(define-minor-mode
+  coe-code-scratch-mode
+  "Mode for scratch.org files in the craft of emacs book.
+  \\{coe-code-scratch-mode-map}"
+  nil
+  nil
+  `((,(kbd "C-c c") . ,#'coe-code-step)
+    (,(kbd "C-c r") . ,#'coe-code-scratch-region)
+    (,(kbd "C-c p") . ,#'coe-code-scratch-point)
+    (,(kbd "C-c e") . ,#'coe-code-export-scratch)
+    (,(kbd "C-c <backspace>") . ,#'coe-code-delete))
+  (message "Mode %s" coe-code-scratch-mode)
+  (if coe-code-scratch-mode
+      ;; Enable coe
+      (progn (add-hook 'before-save-hook #'coe-code-save-scratch 0 t)
+             (coe-code--load-scratch))
+    ;; Disable coe
+    (progn (coe-code-save-scratch)
+           (coe-code--delete-overlays)
+           (remove-hook 'before-save-hook #'coe-code-save-scratch t))))
 
 ;; Book
 
@@ -368,6 +454,13 @@ src-block. The overlaid text is surrounded by symbols depending on its type."
 This is used by yas in pollen-markup-mode to insert a src code snippet."
   (interactive)
   (directory-files (concat default-directory "code")))
+
+(defun coe-code-scratches ()
+  "A list of all the files under the scratch directory.
+
+This is used by yas in pollen-markup-mode to insert a src code snippet."
+  (interactive)
+  (directory-files (concat default-directory "scratch")))
 
 (provide 'coe-code)
 ;;; coe-code.el ends here
